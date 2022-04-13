631b1d87feb22b4011142f999c9ba4cc5624db7e
skyeome <tjru7230@naver.com>
2022-04-13 19:59:03 +0900

    #1.1 useInput

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import { useState, Component } from "react";
+import { useState } from "react";
 
-function App() {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(current => current + 1);
-  const decrementItem = () => setItem(current => current - 1);
-  return (
-    <div className="App">
-      <h1>Count {item}</h1>
-      <button onClick={incrementItem}>UP</button>
-      <button onClick={decrementItem}>DOWN</button>
-    </div>
-  );
+const useInput = (initialValue, validator)=>{
+  //true일때만 value값이 바뀌어야 한다.
+  let willUpdate = true;
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: {value}
+    } = event;
+    //validator라는 함수가 넘어오면 그 함수에서 전달해주는 값에 따라
+    //willUpdate가 변경되어야 한다.
+    if(typeof validator === "function") willUpdate = validator(value);
+    //willUpdate가 true일때만 input의 value를 바꾼다.
+    if(willUpdate) setValue(value);
+  }
+  return { value, onChange };
 }
 
-class AppUgly extends Component{
-  state = {
-    item:1
-  }
-  incrementItem = () => {
-    this.setState (state=>{
-      return {item : state.item + 1};
-    });
-  }
-  decrementItem = () => {
-    this.setState (state=>{
-      return {item : state.item - 1};
-    });
-  }
-  render(){
-    const {item} = this.state;
-    return (
-      <div className="App">
-        <h1>Count {item}</h1>
-        <button onClick={this.incrementItem}>UP</button>
-        <button onClick={this.decrementItem}>DOWN</button>
-      </div>
-    );
-  }
+function App() {
+  //글자수가 10자 이하일때는 true를 리턴한다.
+  const chkLen = (value) => value.length <= 10;
+  const name = useInput("Mr.",chkLen);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input type="text" placeholder="Name" {...name} />
+    </div>
+  );
 }
 
 export default App;
