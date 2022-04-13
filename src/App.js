7730b9e8938f7a51d7e6b6e410f7fc1c04b7d72d
skyeome <tjru7230@naver.com>
2022-04-13 19:06:45 +0900

    useState Before & After

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import { useState, Component } from "react";
+
 function App() {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(current => current + 1);
+  const decrementItem = () => setItem(current => current - 1);
   return (
-    <div></div>
+    <div className="App">
+      <h1>Count {item}</h1>
+      <button onClick={incrementItem}>UP</button>
+      <button onClick={decrementItem}>DOWN</button>
+    </div>
   );
+}
+
+class AppUgly extends Component{
+  state = {
+    item:1
+  }
+  incrementItem = () => {
+    this.setState (state=>{
+      return {item : state.item + 1};
+    });
+  }
+  decrementItem = () => {
+    this.setState (state=>{
+      return {item : state.item - 1};
+    });
+  }
+  render(){
+    const {item} = this.state;
+    return (
+      <div className="App">
+        <h1>Count {item}</h1>
+        <button onClick={this.incrementItem}>UP</button>
+        <button onClick={this.decrementItem}>DOWN</button>
+      </div>
+    );
+  }
 }
 
 export default App;
