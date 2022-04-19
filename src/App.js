a46c2242e4717d31bdd9de14886258428dd69726
skyeome <tjru7230@naver.com>
2022-04-19 12:43:43 +0900

    #2.1 useTItle

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const content = [
-  {
-    tab:"Section 1",
-    content:"I'm the content of Section 1"
-  },
-  {
-    tab:"Section 2",
-    content:"I'm the content of Section 2"
-  }
-];
-
-const useTab = (initialIndex, tabArray)=>{
-  const [currentIndex, setCurrentIndex] = useState(initialIndex);
-  //tabArray가 제공되지 않으면 함수가 실행안되게 강제종료
-  if(!tabArray || !Array.isArray(tabArray)) return;
-  return {
-    currentItem: tabArray[currentIndex],
-    changeCurrentIndex: setCurrentIndex
-  }
-};
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle,[title]);
+  return setTitle;
+}
 
 function App() {
-  const {currentItem,changeCurrentIndex} = useTab(0,content);
-
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(()=>titleUpdater("Home"),3000);
   return (
     <div className="App">
-      {content.map((section,idx)=><button onClick={()=>changeCurrentIndex(idx)}>{section.tab}</button>)}
-      <div>
-        {currentItem.content}
-      </div>
+      
     </div>
   );
 }
